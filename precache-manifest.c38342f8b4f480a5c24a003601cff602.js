f76aeb7b9b259082984bb4b234798a4eb3939445
Sudarshan Suryaprakash <sudarshan@Sudarshans-MacBook-Air.local>
2020-07-17 21:32:17 +0100

    Updates

---
+++ precache-manifest.c38342f8b4f480a5c24a003601cff602.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6398f8c84db1997df399dfe7adbaca48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0210a20c6d5c732aa4e6",
+    "url": "/static/css/main.b0e6e590.chunk.css"
+  },
+  {
+    "revision": "14411bc38dfef5049d0f",
+    "url": "/static/js/2.2673c604.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.2673c604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0210a20c6d5c732aa4e6",
+    "url": "/static/js/main.042bfb41.chunk.js"
+  },
+  {
+    "revision": "4df097abbea25f86dac0",
+    "url": "/static/js/runtime-main.f10a8730.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
